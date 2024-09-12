de3a6e25fe34390a6e0d45de73ea0a6d96a7a338
Buckram <buckram123@gmail.com>
2024-09-12 11:30:02 +0300

    update daemon test

--- contracts/test-migrated/tests/daemon.rs
+++ contracts/test-migrated/tests/daemon.rs
@@ -1,7 +1,7 @@
 mod common;
 
 use cw_blob::interface::CwBlob;
-use cw_orch::prelude::CwOrchUpload;
+use cw_orch::prelude::*;
 use cw_orch_daemon::{networks, Daemon};
 
 // From https://github.com/CosmosContracts/juno/blob/32568dba828ff7783aea8cb5bb4b8b5832888255/docker/test-user.env#L2
@@ -16,7 +16,7 @@ fn daemon_local() {
         .unwrap();
 
     let blob = CwBlob::new("blob", daemon.clone());
-    blob.upload();
+    blob.upload().unwrap();
 
-    common::test(daemon, blob)
+    common::test(daemon, blob.code_id().unwrap())
 }
