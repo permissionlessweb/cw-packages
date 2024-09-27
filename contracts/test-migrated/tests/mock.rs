20200591652cbb03d0086cf555a5c596568cbec0
Buckram <buckram123@gmail.com>
2024-09-27 11:38:52 +0300

    clone test

--- contracts/test-migrated/tests/mock.rs
+++ contracts/test-migrated/tests/mock.rs
@@ -1,10 +1,19 @@
 mod common;
 
 use cw_orch::{anyhow, mock::MockBech32};
+use cw_orch_clone_testing::CloneTesting;
+use cw_orch_daemon::networks;
 
 #[test]
 fn mock() -> anyhow::Result<()> {
     let chain = MockBech32::new("cosm");
+
+    common::test(chain)
+}
+
+#[test]
+fn clone_test() -> anyhow::Result<()> {
+    let chain = CloneTesting::new(networks::JUNO_1)?;
 
     common::test(chain)
 }
